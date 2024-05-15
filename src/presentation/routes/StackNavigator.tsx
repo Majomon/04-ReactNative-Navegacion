dc358f55920e2231627a09928b3f3d6ade2b80b4
Mauri <mauri.monzon91@gmail.com>
2024-05-15 11:29:46 -0300

    OPcions del navigator

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -1,14 +1,22 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import { HomeScreen } from '../screens/home/HomeScreen';
-import { ProductsScreen } from '../screens/products/ProductsScreen';
-import { SettingsScreen } from '../screens/settings/SettingsScreen';
+import {HomeScreen} from '../screens/home/HomeScreen';
+import {ProductsScreen} from '../screens/products/ProductsScreen';
+import {SettingsScreen} from '../screens/settings/SettingsScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          elevation: 50,
+          shadowColor: 'transparent',
+          // shadowColor: 'blue',
+        },
+      }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Products" component={ProductsScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
