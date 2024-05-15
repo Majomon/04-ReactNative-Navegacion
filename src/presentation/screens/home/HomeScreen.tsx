61353965e7b146e78724a7c33456c78b44d14a85
Mauri <mauri.monzon91@gmail.com>
2024-05-15 16:54:15 -0300

    Mostrar/ocultar drawer

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Pressable, Text, View} from 'react-native';
 import {globalStyles} from '../../theme/theme';
-import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  DrawerActions,
+  NavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
 import {PrimaryButton} from '../../components';
 import {RootsStackParams} from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootsStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text style={{color: 'black', fontSize: 16}}>Menu</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
+
   return (
     <View style={globalStyles.container}>
       <PrimaryButton
