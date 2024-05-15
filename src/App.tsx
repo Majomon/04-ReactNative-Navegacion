aec9b76ff4783e324d076d9b91a4c7beb7774ec3
Mauri <mauri.monzon91@gmail.com>
2024-05-15 10:21:46 -0300

    Hasta aca funciona con el navigator

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
+
+export const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>App</Text>
+    </NavigationContainer>
+  );
+};
--- src/presentation/theme/theme.ts
+++
@@ -1,5 +0,0 @@
-import {StyleSheet} from 'react-native';
-
-export const colors = {};
-
-export const globalStyles = StyleSheet.create({});
