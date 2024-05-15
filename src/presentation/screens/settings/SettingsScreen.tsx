2456544e77df206b547e89bf7eed0375351bd984
Mauri <mauri.monzon91@gmail.com>
2024-05-15 15:40:33 -0300

    Regresar al home

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {PrimaryButton} from '../../components';
+import {StackActions, useNavigation} from '@react-navigation/native';
 
 export const SettingsScreen = () => {
+  const navigator = useNavigation();
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.text}>SettingsScreen</Text>
+
+      <PrimaryButton onPress={() => navigator.goBack()} label="Regresar" />
+      <PrimaryButton
+        onPress={() => navigator.dispatch(StackActions.popToTop())}
+        label="Regresar al home"
+      />
     </View>
   );
 };
