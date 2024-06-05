f9571912302ed32df8919403febf8e43debbd33e
Mauri <mauri.monzon91@gmail.com>
2024-06-05 11:09:09 -0300

    Instalando iconos

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { HamburguerMenu } from '../../components/shared/HamburguerMenu';
+import {Text, View} from 'react-native';
+import {HamburguerMenu} from '../../components/shared/HamburguerMenu';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const Tab1Screen = () => {
-
-
   return (
     <View>
-      <HamburguerMenu/>
-      <Text>Tab1Screen</Text>
+      <HamburguerMenu />
+      <Text style={{color:"black"}}>Tab1Screen</Text>
+      <Icon name="rocket-outline" size={30} color={'#900'} />
     </View>
   );
 };
